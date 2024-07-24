af9c6e1583c326aec07fa0acbb01bb1a16bb1423
Hoakyung <hoakyung@hotmail.com>
2024-07-24 12:36:02 +0100

    Added javascript file

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,13 @@
+//1.window:alert() and document.write() methods.
+window.alert("Hello, world!");
+document.write("I love you!");
+
+//2. Two variables using Sent 1 and Sent2.
+var Sent1 = "This is variable using Sent 1.";
+var Sent2 = "This is variable using Sent 2.";
+
+//3. Concatenating the Sent1 and Sent 2.
+document.write(Sent1 + Sent2);
+
+//4. writing an expressing
+document.write(4-2);
