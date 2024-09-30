dbad2b5c85b6ad1a94d2eb719f5486a623f0a2d1
Hoakyung <hoakyung@hotmail.com>
2024-09-30 17:59:13 +0100

    Assignment using the += operator
    JavaScript code using += operator has been added

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,6 @@
+//JavaScript code using function += operator
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
